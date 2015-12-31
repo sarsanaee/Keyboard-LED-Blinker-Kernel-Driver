e97f8a4cb487a49b0028eb08fdb8d245a449da94
alireza sanaee <sarsanaee@gmail.com>
2015-12-31 18:09:54 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@ rmmod mydev
 make
 insmod mydev.ko
 rm /dev/mysimpledriver
-mknod /dev/mysimpledriver c 246 0
+mknod /dev/mysimpledriver c 246 0 #be sure to check the correct number from : cat /proc/devices | grep eadriver
 echo "test" > /dev/mysimpledriver
 cat /dev/mysimpledriver

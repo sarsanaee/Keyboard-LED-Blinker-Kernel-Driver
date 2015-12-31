18eef576f2b8937f72e184d4cbd3a934052b8216
alireza sanaee <sarsanaee@gmail.com>
2015-12-31 18:08:17 +0330

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,5 @@ make
 insmod mydev.ko
 rm /dev/mysimpledriver
 mknod /dev/mysimpledriver c 246 0
-#echo "alireza sanae is someone mad in computer science" > /dev/mysimpledriver
 echo "test" > /dev/mysimpledriver
 cat /dev/mysimpledriver
